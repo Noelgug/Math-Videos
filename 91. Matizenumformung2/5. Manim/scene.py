919e45e8e781c9890dee04e8062f48664210d304
Noelgug <noelgug@gmail.com>
2024-11-26 09:04:34 +0100

    Alle Dateien ignorieren

--- 91. Matizenumformung2/5. Manim/scene.py
+++ 91. Matizenumformung2/5. Manim/scene.py
@@ -7,7 +7,7 @@ from Resourcen.colors import *
 class Einheitsmatrix(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
-        Mobject.set_default(color=MMNMAINBLUE)
+        Mobject.set_default(color=BLACK)
         MyTexTemplate = TexTemplate(
             tex_compiler="xelatex",
             output_format=".xdv",
@@ -43,19 +43,28 @@ class Einheitsmatrix(Scene):
         self.wait(2)
 
         # Füge Pfeile und Beschriftungen hinzu
-        row_arrow = Arrow(start=RIGHT*4, end=LEFT*1, color=RED).shift(UP*0.2)
-        col_arrow = Arrow(start=UP*2, end=DOWN*1, color=BLUE).shift(LEFT*0.5, RIGHT*1.5)
+        row_arrow = Arrow(start=LEFT*1, end=RIGHT*0.9, color=MMNMAINRED).shift(UP*0.7)
+        col_arrow = Arrow(start=UP*2, end=UP*0, color=MMNMAINLIGHTBLUE).shift(RIGHT*0.8, UP*0.7)
 
-        row_label = MathTex("i").next_to(row_arrow, RIGHT)
-        col_label = MathTex("j").next_to(col_arrow, UP)
+        row_label = MathTex("i").next_to(row_arrow, LEFT).set_color(MMNMAINRED)
+        col_label = MathTex("j").next_to(col_arrow, UP).set_color(MMNMAINLIGHTBLUE)
+
+        #for i, part in enumerate(matrix):
+        #    print(f"Index {i}: {part.get_tex_string}")
+
 
         # Animation
         self.wait()
         self.play(
             GrowArrow(row_arrow),
-            GrowArrow(col_arrow),
             Write(row_label),
-            Write(col_label)
+            FadeToColor(matrix[0][1], MMNMAINRED),
+        )
+        self.wait()
+        self.play(
+            GrowArrow(col_arrow),
+            Write(col_label),
+            FadeToColor(matrix[0][3], MMNMAINLIGHTBLUE),
         )
         self.wait()
 
@@ -70,15 +79,15 @@ class Einheitsmatrix(Scene):
             r"\end{pmatrix}"
         ).shift(UP)
 
-        """ self.play(Transform(matrix, matrix2),
+        self.play(Transform(matrix, matrix2),
                   FadeOut(text),
                   FadeOut(formula1))
-        self.wait(2) """
+        self.wait(2)
 
 
 
         # dritte Zeilentransformation:
-        # vertauschen von zweier Zeilen
+        # vertauschen von zwei Zeilen
         matrix_5x5 = MathTex(
             r"S_{ij} = \begin{pmatrix}"
             r"1 & * & * & * & * \\"
@@ -90,7 +99,7 @@ class Einheitsmatrix(Scene):
         ).shift(UP)
 
         # Add the 5x5 matrix to the scene
-        """ self.play(Transform(matrix, matrix_5x5))
-        self.wait(2) """
+        self.play(Transform(matrix, matrix_5x5))
+        self.wait(2)
         
         # Add the matrix to the scene
